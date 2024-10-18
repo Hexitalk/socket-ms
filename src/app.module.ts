b958db29d5c3c1bb3e723ab9ee1b26dddaf328a0
Jorge Estébenez <jorgecodesync@gmail.com>
2024-10-18 02:29:48 +0200

    health check controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AuthModule } from './contexts/auth/infraestructure/auth.module';
 import { SocketModule } from './contexts/socket/infraestructure/socket.module';
 import { HubModule } from './contexts/hub/infraestructure/hub.module';
 import { ChatModule } from './contexts/chat/infraestructure/chat.module';
+import { HealthCheckModule } from './contexts/health-check/health-check.module';
 
 @Module({
-  imports: [SocketModule, ProfilesModule, AuthModule, HubModule, ChatModule],
+  imports: [
+    SocketModule,
+    ProfilesModule,
+    AuthModule,
+    HubModule,
+    ChatModule,
+    HealthCheckModule,
+  ],
   controllers: [],
   providers: [],
 })
